9f7d8cd1460bda29fe7779342e9890548e05e20f
Linga-Sunvith <sunvithlinga22@gmailcom>
2025-12-08 16:13:51 +0530

    Fix root build file for Maven Central publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
     alias(libs.plugins.android.library) apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.vanniktech.maven.publish") version "0.28.0"
-}
-nexusPublishing {
-    repositories {
-        sonatype {
-            username.set(System.getenv("OSSRH_USERNAME"))
-            password.set(System.getenv("OSSRH_PASSWORD"))
-        }
-    }
 }

632930a86d539c5b773efe975208bb5d29753b6a
Linga-Sunvith <sunvithlinga22@gmailcom>
2025-12-08 12:04:18 +0530

    Add nexus publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
     alias(libs.plugins.android.library) apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
+nexusPublishing {
+    repositories {
+        sonatype {
+            username.set(System.getenv("OSSRH_USERNAME"))
+            password.set(System.getenv("OSSRH_PASSWORD"))
+        }
+    }
 }

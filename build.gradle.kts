915b9e2814911666a82c985de0b6721dac78db07
Linga-Sunvith <sunvithlinga22@gmail.com>
2025-12-19 12:44:02 +0530

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.library) apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.vanniktech.maven.publish") version "0.28.0"
-}
